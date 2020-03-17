94f3a4bb0228019ed91b171974e13168db648ee3
Marius Phoenix <phoenixzenomarius@gmail.com>
2020-03-17 15:46:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -76,7 +76,7 @@ def monopoly_with_fourth_tier
           :mortgage_value => 400
         },
         :shortline_railroad => {
-          :mort
+          :mortgage_value => 800
         }
       }
     }
